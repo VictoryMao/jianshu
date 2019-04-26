95adb2c33368de74e2314b5532d37a996e628a1a
VictoryMao <1415088671@qq.com>
2019-04-26 18:56:20 +0800

    头部布局

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './common/header';
 import { GlobalStyle } from "./reset";
 function App() {
   return (
       <div>
          <GlobalStyle />
-          <div>略略略</div>
+         <Header></Header>
       </div>
   );
 }
